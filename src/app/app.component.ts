ecdb13f3fa0dcc6feec3caca90de30038e78dcce
RobertoABernasconi <roberto.a.bernasconi@gmail.com>
2025-05-26 16:42:50 +0200

    Primo commit: inserimento immagine, data binding non funziona ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    //RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  count = 0;
+  title = 'Data Binding';
+  picturePath: string = "icon.png";
+  currentText = '';
+  numbers = ['uno', 'due', 'tre', 'quattro', 'cinque'];
+  check($event: MouseEvent) {
+    const buttonClicked = $event.target as HTMLInputElement;
+    if (this.currentText == 'disattiva') {
+      buttonClicked.disabled = true;
+      this.currentText = 'bottone disattivato con successo';
+    }
+  }
+  passValueToComponent(e: Event) {
+    const inputElement = e.target as HTMLInputElement
+    this.currentText = inputElement.value;
+  }
+
+  increment() {
+    this.count++;
+  }
+
 }
