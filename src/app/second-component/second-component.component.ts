4ef39dacbab232447ab23b8bbde4060c27dfafc4
RobertoABernasconi <roberto.a.bernasconi@gmail.com>
2025-06-19 13:20:15 +0200

    aggiunti valori iniziali al secondo componente

--- src/app/second-component/second-component.component.ts
+++ src/app/second-component/second-component.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
 })
 
 export class SecondComponentComponent {
-  numbers: string[] = [];
+  numbers: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"];
   populateDivisibleBy(x: number) {
     for (let i = 0; i < 25; i++) {
       if (Math.floor((i+1) % x) == 0) {
